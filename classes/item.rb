765fdafe3f05c80e4de9235d99227702c4162db5
j-tee <juliustetteh@gmail.com>
2023-01-09 16:46:40 +0000

    Created the setter method for source variable

--- classes/item.rb
+++ classes/item.rb
@@ -1,6 +1,6 @@
 class Item
   # Getter
-  attr_reader :genre
+  attr_reader :genre, :source
 
   def initialize(publish_date)
     @id = Random.rand(1..1000)
@@ -11,5 +11,10 @@ class Item
   def add_genre(genre)
     @genre = genre
     genre.items << self unless genre.items.include?(self)
+  end
+
+  def add_source(source)
+    @source = source
+    source.items << self unless source.items.include?(self)
   end
 end
