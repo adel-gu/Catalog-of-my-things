b2a78a34a84db723633f8ec174e34f694824b5bb
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-01-10 13:40:27 +0000

    Test the functionality of the add_item method

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../classes/label'
+require_relative '../classes/item'
 
 describe Label do
   before :each do
@@ -8,6 +9,14 @@ describe Label do
   describe '#new' do
     it 'should take two arguments and create a Label object' do
       expect(@label).to be_an_instance_of Label
+    end
+  end
+
+  describe '#add_item' do
+    it 'should return a non-empty items array' do
+      item = Item.new('2022-04-21')
+      @label.items << item
+      expect(@label.items.empty?).to be false
     end
   end
 end
