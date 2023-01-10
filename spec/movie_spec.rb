71f4514d8bac9e5dd879bf74ad88d7b656409ea0
j-tee <juliustetteh@gmail.com>
2023-01-10 13:39:38 +0000

    Override can_be_archived? method in the Item class

---
+++ spec/movie_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../classes/movie'
+
+movie = Movie.new('2020/2/2', true)
+
+describe '#Review the Movie file' do
+  it 'returns true' do
+    expect(movie.silent).to eql true
+  end
+
+  it 'returns true' do
+    expect(movie.can_be_archived?).to eql true
+  end
+end
