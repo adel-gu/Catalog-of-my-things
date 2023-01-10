8a18f1617621d72f47517dfe1367bd5660d60a69
adelguit <adeelguitoun@gmail.com>
2023-01-10 16:31:12 +0100

    Update the on_spotify to be a keyword argument in the constructor

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,14 +1,15 @@
-require_relative "./item"
+require_relative './item'
 
 class MusicAlbum < Item
   attr_accessor :on_spotify
 
-  def initialize(publish_date, on_spotify)
+  def initialize(publish_date, on_spotify:true)
     super(publish_date)
     @on_spotify = on_spotify
   end
 
   private
+
   def can_be_archived?
     super && @on_spotify
   end
