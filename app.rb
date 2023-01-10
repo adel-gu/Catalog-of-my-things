0bb75a7acc1ed9934d05ccf6186734094d50f12f
j-tee <juliustetteh@gmail.com>
2023-01-10 09:25:14 +0000

    Implemented get user input

--- app.rb
+++ app.rb
@@ -1,29 +1,23 @@
 class App
   def prompt()
+    puts 'Select an option (Enter a number)'
+    puts
     puts "
+    1 - List all books \n
+    2 - List all music albums \n
+    3 - List all movies \n
+    4 - List of games \n
+    5 - List all genres (e.g 'Comedy', 'Thriller') \n
+    6 - List all labels (e.g. 'Gift', 'New') \n
+    7 - List all authors (e.g. 'Stephen King') \n
+    8 - List all sources (e.g. 'From a friend', 'Online shop') \n
+    9 - Add a book \n
+    10 - Add a music album \n
+    11 - Add a movie \n
+    12 - Add a game \n "
+  end
 
-    List all books \n
-
-    List all music albums \n
-
-    List all movies \n
-
-    List of games \n
-
-    List all genres (e.g 'Comedy', 'Thriller') \n
-
-    List all labels (e.g. 'Gift', 'New') \n
-
-    List all authors (e.g. 'Stephen King') \n
-
-    List all sources (e.g. 'From a friend', 'Online shop') \n
-
-    Add a book \n
-
-    Add a music album \n
-
-    Add a movie \n
-
-    Add a game \n "
+  def get_input
+    gets.chomp.to_i
   end
 end
--- main.rb
+++ main.rb
@@ -1,8 +1,9 @@
-require 'app'
+require_relative './app'
 
 def main
   app = App.new
   app.prompt
+  app.get_input
 end
 
 main
