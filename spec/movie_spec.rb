51c335241d273b0f9ef497b1dc82d25fff6f1ba9
j-tee <juliustetteh@gmail.com>
2023-01-10 13:52:32 +0000

    Fix failing unit tests

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -4,10 +4,10 @@ movie = Movie.new('2020/2/2')
 
 describe '#Review the Movie file' do
   it 'returns true' do
-    expect(movie.silent).to eql true
+    expect(movie.silent).to eql false
   end
 
   it 'returns true' do
-    expect(movie.can_be_archived?).to eql true
+    expect(movie.can_be_archived?).to eql false
   end
 end
