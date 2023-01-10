e83cf7f3350cc54ae5ddd6a934fba8621fe93f54
RichardChileya <richardchileyawk@gmail.com>
2023-01-10 18:55:29 +0200

    Fix Linters errors

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,11 +1,11 @@
 require_relative '../classes/game'
 
-# describe Game do 
+# describe Game do
 #   before(:each) do
 #    @game = Game.new('2023-01-01', '2020-01-01', true)
 #    @game = Game.new('2020-01-01', '2018-01-01', true)
 
-#    context 'test multple player' do 
+#    context 'test multple player' do
 #     it ' should return true or false'
 #       expect(@game.multiple_player).to eq true
 #    end
@@ -17,7 +17,6 @@ describe Game do
   end
 
   context 'create game' do
-
     it 'check multiplayer' do
       expect(@game.multiplayer).to eq true
     end
@@ -25,7 +24,7 @@ describe Game do
     it 'check when last played' do
       expect(@game.last_played_at).to eq '2023-01-01'
     end
-    
+
     it 'check if it can be archived' do
       @game.can_be_archived?
       expect(@game.can_be_archived?).to eq false
