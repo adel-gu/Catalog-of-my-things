f31916b46ac47e2923a8ea758e76d6a15de1bc3d
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-01-10 15:06:08 +0000

    Add test to check if Book inherits from Item

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,5 +1,20 @@
+require_relative '../classes/item'
 require_relative '../classes/book'
 
 describe Book do
-  
+  # before :each do
+  #   @book = Book.new('2022-04-21')
+  # end
+
+  # describe '#new' do
+  #   it 'should return an instance of Book' do
+  #     expect(@book).to be_an_instance_of Book
+  #   end
+  # end
+
+  describe '< Item' do
+    it 'should inherit from Item class' do
+      expect(Book).to be < Item
+    end
+  end
 end
