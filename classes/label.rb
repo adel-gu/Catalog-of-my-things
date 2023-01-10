b7eaaff53327ad4aa1312fd89036fdb2393f1ab3
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-01-10 13:05:36 +0000

    Add initialize method to pass test

--- classes/label.rb
+++ classes/label.rb
@@ -1,3 +1,9 @@
-class Label 
-  
+class Label
+  attr_reader :id, :title, :color
+
+  def initialize(title, color)
+    @id = Random.rand(1..1000)
+    @title = title
+    @color = color
+  end
 end
