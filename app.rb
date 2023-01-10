239d1d1086b4de0b832e3e4ab346e8d5081c4a1d
RichardChileya <richardchileyawk@gmail.com>
2023-01-10 11:01:35 +0200

    Add main UI file and App UI file

---
+++ app.rb
@@ -0,0 +1,29 @@
+class App
+  def prompt()
+    puts "
+
+    List all books \n
+
+    List all music albums \n
+
+    List all movies \n
+
+    List of games \n
+
+    List all genres (e.g 'Comedy', 'Thriller') \n
+
+    List all labels (e.g. 'Gift', 'New') \n
+
+    List all authors (e.g. 'Stephen King') \n
+
+    List all sources (e.g. 'From a friend', 'Online shop') \n
+
+    Add a book \n
+
+    Add a music album \n
+
+    Add a movie \n
+
+    Add a game \n "
+  end
+end
---
+++ main.rb
@@ -0,0 +1,8 @@
+require 'app'
+
+def main
+  app = App.new
+  app.prompt
+end
+
+main
