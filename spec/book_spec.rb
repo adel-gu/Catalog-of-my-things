0cf3b146d555a2f45064e2cc29a5b26af0aad7fc
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-01-10 15:16:05 +0000

    Add test to check initialization of Book class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,15 +2,15 @@ require_relative '../classes/item'
 require_relative '../classes/book'
 
 describe Book do
-  # before :each do
-  #   @book = Book.new('2022-04-21')
-  # end
+  before :each do
+    @book = Book.new('2022-04-21')
+  end
 
-  # describe '#new' do
-  #   it 'should return an instance of Book' do
-  #     expect(@book).to be_an_instance_of Book
-  #   end
-  # end
+  describe '#new' do
+    it 'should return an instance of Book' do
+      expect(@book).to be_an_instance_of Book
+    end
+  end
 
   describe '< Item' do
     it 'should inherit from Item class' do
