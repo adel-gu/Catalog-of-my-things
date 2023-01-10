91aacc1f77324c864130fa348bb4466d3bb45026
adelguit <adeelguitoun@gmail.com>
2023-01-10 14:48:29 +0100

    Fix linters errors

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -3,15 +3,15 @@ require_relative '../classes/item'
 
 describe Genre do
   before(:each) do
-    @item = Item.new("2023-01-10")
+    @item = Item.new('2023-01-10')
   end
 
-  context "When testing the Genre class" do
-     it "should add an item to the genre items when we call the add_items method" do
-        genre = Genre.new("Comedy")
-        expect(genre.items.length).to eq 0
-        genre.add_items(@item)
-        expect(genre.items.length).to eq 1
-     end
+  context 'When testing the Genre class' do
+    it 'should add an item to the genre items when we call the add_items method' do
+      genre = Genre.new('Comedy')
+      expect(genre.items.length).to eq 0
+      genre.add_items(@item)
+      expect(genre.items.length).to eq 1
+    end
   end
 end
