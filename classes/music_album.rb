a61ad9c9cac41b85ee4e14600b79fc7de7c13ec2
adelguit <adeelguitoun@gmail.com>
2023-01-10 15:56:57 +0100

    Implement the MusicAlbum class

---
+++ classes/music_album.rb
@@ -0,0 +1,15 @@
+require_relative "./item"
+
+class MusicAlbum < Item
+  attr_accessor :on_spotify
+
+  def initialize(publish_date, on_spotify)
+    super(publish_date)
+    @on_spotify = on_spotify
+  end
+
+  private
+  def can_be_archived?
+    super && @on_spotify
+  end
+end
