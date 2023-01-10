e9c21cd5e2305ce854f9187f6b9a17005a62b4a3
adelguit <adeelguitoun@gmail.com>
2023-01-10 16:31:49 +0100

    Implement unit testing for the MusicAlbum class

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -2,17 +2,28 @@ require_relative '../classes/music_album'
 
 describe MusicAlbum do
   before(:each) do
-     @musicAlbum = MusicAlbum.new("2010-01-01", true)
+    @music_album = MusicAlbum.new('2010-01-01', on_spotify: true)
   end
 
-  context "testing the move_to_archive method" do
-    it 'should return true if on_spotify true' do
-      expect(@musicAlbum.move_to_archive).to eq true
+  context 'testing the move_to_archive method' do
+    it 'should return true if on_spotify true and the published year is over 10 years' do
+      expect(@music_album.move_to_archive).to eq true
     end
 
-    it 'should return false if on_spotify false' do
-      @musicAlbum.on_spotify = false
-      expect(@musicAlbum.move_to_archive).to eq false
+    it 'should return false if on_spotify false even when the published year is over 10 years' do
+      @music_album.on_spotify = false
+      expect(@music_album.move_to_archive).to eq false
+    end
+
+    it 'should set the archived variable to true if on_spotify true and the published year is over 10 years' do
+      @music_album.move_to_archive
+      expect(@music_album.archived).to eq true
+    end
+
+    it 'should set the archived variable to false if on_spotify false even when the published year is over 10 years' do
+      @music_album.on_spotify = false
+      @music_album.move_to_archive
+      expect(@music_album.archived).to eq false
     end
   end
 end
