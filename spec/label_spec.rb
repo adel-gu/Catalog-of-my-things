fcdb2018fbaeb4ffb9db3ceeae43421dd2e7827a
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-01-10 13:03:02 +0000

    Add test to check initialization of Label class

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,5 +1,13 @@
 require_relative '../classes/label'
 
 describe Label do
-  
+  before :each do
+    @label = Label.new('In the chest of a woman', 'Violet')
+  end
+
+  describe '#new' do
+    it 'should take two arguments and create a Label object' do
+      expect(@label).to be_an_instance_of Label
+    end
+  end
 end
