95d3a7b7af2d7526b66d02ef65deddc3ff3a2dfc
j-tee <juliustetteh@gmail.com>
2023-01-11 10:46:52 +0000

    Fix linters error

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -5,10 +5,10 @@ describe 'Movie Class' do
     @movie = Movie.new('2020/2/2')
   end
   it 'returns true' do
-    expect(movie.silent).to eql false
+    expect(@movie.silent).to eql false
   end
 
   it 'returns true' do
-    expect(movie.move_to_archive?).to eql false
+    expect(@movie.move_to_archive?).to eql false
   end
 end
