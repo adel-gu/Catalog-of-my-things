70b23627f04e995ba6be61b33d7f2b7579390e78
j-tee <juliustetteh@gmail.com>
2023-01-11 12:46:29 +0000

    Fix folder struture and unit tests

--- classes/modules/database_write.rb
+++
@@ -1,7 +0,0 @@
-require 'json'
-
-module DatabaseWrite
-  def write_movie(movies)
-    File.write('./json/movies.json', JSON.pretty_generate(movies))
-  end
-end
--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -2,17 +2,14 @@ require_relative '../classes/source'
 require_relative '../classes/item'
 
 describe Source do
-  before :each do
-    @source = Source.new('Online')
-  end
-
   describe '#new' do
+    before :each do
+      @source = Source.new('Online')
+    end
     it 'should take two arguments and create a Source object' do
       expect(@source).to be_an_instance_of Source
     end
-  end
 
-  describe '#add_item' do
     it 'should return a non-empty items array' do
       item = Item.new('2022-04-21')
       @source.items << item
