6f4c8dc50347442f3ffc4a86d7312f12f8114eb1
j-tee <juliustetteh@gmail.com>
2023-01-11 10:42:37 +0000

    Fix linters error

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -1,10 +1,11 @@
 require_relative '../classes/movie'
 
+describe 'Movie Class' do
+  
 before(:each) do
   @movie = Movie.new('2020/2/2')
 end
 
-describe 'Movie Class' do
   it 'returns true' do
     expect(movie.silent).to eql false
   end
