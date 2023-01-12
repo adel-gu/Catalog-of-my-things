70f1a9a3e0876a438830b0b516b2bd947976daa4
adelguit <adeelguitoun@gmail.com>
2023-01-12 16:35:37 +0100

    Create music_album table schema

--- database/schema.sql
+++ database/schema.sql
@@ -4,3 +4,35 @@ CREATE TABLE genres (
     name VARCHAR(150),
     PRIMARY KEY (id)
 );
+
+-- Music-Album table
+CREATE TABLE music_albums (
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    publish_date DATE,
+    on_spotify BOOLEAN,
+    archived BOOLEAN,
+    genre_id INT,
+    genre_name VARCHAR(150),
+    author_id INT,
+    author_first_name VARCHAR(150),
+    author_last_name VARCHAR(150),
+    source_id INT,
+    source_name VARCHAR(150),
+    label_id INT,
+    label_name VARCHAR(150),
+    label_color VARCHAR(150),
+
+    PRIMARY KEY (id),
+    CONSTRAINT fk_genres
+        FOREIGN KEY (genres_id)
+            REFERENCES genres(id),
+    CONSTRAINT fk_author
+        FOREIGN KEY (author_id)
+            REFERENCES  author(id),
+    CONSTRAINT fk_source
+        FOREIGN KEY (source_id)
+            REFERENCES  source(id),
+    CONSTRAINT fk_label
+        FOREIGN KEY (label_id)
+            REFERENCES  label(id)
+);
