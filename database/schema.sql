0b7e43953d917bf2a014b553edf948c8c3997fe1
RichardChileya <richardchileyawk@gmail.com>
2023-01-13 14:05:05 +0200

    Fix Author and Game Table foreign key attributes

--- database/schema.sql
+++ database/schema.sql
@@ -3,10 +3,8 @@ CREATE TABLE authors (
   id INT GENERATED ALWAYS AS IDENTITY,
   first_name VARCHAR(30),
   second_name VARCHAR(30),
-  items VARCHAR(100),
   PRIMARY KEY(id),
-   FOREIGN KEY(items) REFERENCES(Item)
-  )
+  );
 
 -- Create Game Table
 CREATE TABLE games (
@@ -14,6 +12,14 @@ CREATE TABLE games (
   archived BOOLEAN,
   multiplayer BOOLEAN,
   last_played_at DATE,
+  label_id INT, 
+  genre_id INT, 
+  source_id INT,
+  author_id INT,
   PRIMARY KEY(id),
-)
+   FOREIGN KEY(author_id) REFERENCES authors(id),
+   FOREIGN KEY(genre_id) REFERENCES genres(id),
+   FOREIGN KEY(source_id) REFERENCES sources(id),
+   FOREIGN KEY(label_id) REFERENCES labels(id)
+);
 
